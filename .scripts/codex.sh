e9a6e57750780af724f7725c125f68a09759721f
ha-sante <90225652+ha-sante@users.noreply.github.com>
2025-09-06 12:32:41 +0000

    wip

--- .scripts/codex.sh
+++
@@ -1,6 +0,0 @@
-export OPENAI_API_KEY='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'
-
-if [ $# -gt 0 ]; then
-  codex "$@"
-fi
-
